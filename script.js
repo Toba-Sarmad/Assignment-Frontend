52679cfed60aeb9ccf423287e67d65193abcadc0
Toba Sarmad <toba.sarmad@learnet.se>
2025-11-28 14:39:49 +0100

    Add category API endpoints for fetching products

---
+++ script.js
@@ -0,0 +1,9 @@
+//API endpoints per kategori
+const categoryAPI = {
+    snacks: "https://world.openfoodfacts.org/api/v2/search?categories_tags=snacks",
+    milk: "https://world.openfoodfacts.org/api/v2/search?categories_tags=milk",
+    bread: "https://world.openfoodfacts.org/api/v2/search?categories_tags=bread",
+    fruits: "https://world.openfoodfacts.org/api/v2/search?categories_tags=fruits",
+    meat: "https://world.openfoodfacts.org/api/v2/search?categories_tags=meat",
+    drinks: "https://world.openfoodfacts.org/api/v2/search?categories_tags=beverages",
+};
